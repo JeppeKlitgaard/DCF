fde20930c1df6ceb215b9a7aeb85c80a3d924593
Jeppe Klitgaard <hi@jeppe.science>
2021-11-15 17:50:21 +0000

    fix: diameter calculation

--- dcf/ddm/__init__.py
+++ dcf/ddm/__init__.py
@@ -11,8 +11,10 @@ def make_diffusion_coefficient_calculator(viscosity: float, temperature: float) 
     return diffusion_coefficient_calculator
 
 
-def make_diameter_calculator(viscosity: float, temperature: float) -> Callable[[float], float]:
+def make_diameter_calculator(viscosity: float, temperature: float, pix2mum: float, framerate: float) -> Callable[[float], float]:
+    scaling_factor = (pix2mum ** 2) * framerate
+
     def diameter_calculator(diffusion_coefficient: float) -> float:
-        return k_B * temperature / (3 * pi * viscosity * diffusion_coefficient) * 1.0e12
+        return k_B * temperature / (3 * pi * viscosity * scaling_factor * diffusion_coefficient) * 1.0e12
 
     return diameter_calculator
