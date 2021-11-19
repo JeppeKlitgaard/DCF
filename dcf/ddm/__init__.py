7c5dc60100140e07d742330402255e2a86641f3c
Jeppe Klitgaard <hi@jeppe.science>
2021-11-19 00:17:56 +0000

    feat: more data

--- dcf/ddm/__init__.py
+++ dcf/ddm/__init__.py
@@ -1,20 +1,13 @@
-from typing import Callable
 from dcf.constants import k_B
 
 from math import pi
 
 
-def make_diffusion_coefficient_calculator(viscosity: float, temperature: float) -> Callable[[float], float]:
-    def diffusion_coefficient_calculator(diameter: float) -> float:
-        return k_B * temperature / (3 * pi * viscosity * diameter) * 1.0e12
+def diffusion_coefficient_calculator(diameter: float, viscosity: float, temperature: float) -> float:
+    return k_B * temperature / (3 * pi * viscosity * diameter) * 1.0e12
 
-    return diffusion_coefficient_calculator
-
-
-def make_diameter_calculator(viscosity: float, temperature: float, pix2mum: float, framerate: float) -> Callable[[float], float]:
+def diameter_calculator(diffusion_coefficient: float, viscosity: float, temperature: float, pix2mum: float, framerate: float) -> float:
     scaling_factor = (pix2mum ** 2) * framerate
+    diameter = k_B * temperature / (3 * pi * viscosity * scaling_factor * diffusion_coefficient) * 1.0e12
 
-    def diameter_calculator(diffusion_coefficient: float) -> float:
-        return k_B * temperature / (3 * pi * viscosity * scaling_factor * diffusion_coefficient) * 1.0e12
-
-    return diameter_calculator
+    return diameter
