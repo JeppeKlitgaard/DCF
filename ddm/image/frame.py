d84267b5dfd14525bec42d2c1cc0d668ed92ad0f
Jeppe Klitgaard <hi@jeppe.science>
2021-11-19 16:06:04 +0000

    feat: data and video renderer

--- ddm/image/frame.py
+++ ddm/image/frame.py
@@ -1,5 +1,15 @@
+from typing import Iterable
 from PIL import Image
+import numpy as np
 
-Frame = Image
-Framestack = list[Frame]
+CddmFormat = Iterable[tuple[np.ndarray]]
+class Framestack:
+    def __init__(self, frames: Iterable[Image.Image]):
+        self.frames = frames
+
+    def to_cddm_format(self) -> CddmFormat:
+        for frame in self.frames:
+            yield (np.array(frame),)
+
+
 
--- ddm/image/reader.py
+++ ddm/image/reader.py
@@ -2,20 +2,20 @@
 Reads frame files
 """
 
-from ddm.image.frame import Frame, Framestack
+from ddm.image.frame import Framestack
 from pathlib import Path
 from PIL import Image
 
-def read_frame(img_path: Path) -> Frame:
+def read_frame(img_path: Path) -> Image.Image:
     im = Image.open(img_path)
 
     return im
 
 
 def read_experiment(data_path: Path, ext="pgm") -> Framestack:
-    stack: Framestack = []
+    frames: list[Image.Image] = []
 
     for img_path in data_path.glob(f"*.{ext}"):
-        stack.append(read_frame(img_path))
+        frames.append(read_frame(img_path))
 
-    return stack
+    return Framestack(frames)
